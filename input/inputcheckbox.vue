6df80dee3ce16012bcf4ea3b9c75c3e596c95d7f
Julien de Charentenay, PhD <julien.decharentenay@gmail.com>
2023-08-16 16:47:56 +0100

    Add checkbox

---
+++ input/inputcheckbox.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex items-start">
+    <div class="ml-3 h-6 items-center">
+      <input id="my-checkbox" name="my-checkbox" type="checkbox" 
+             class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+             v-model="value">
+    </div>
+    <div class="text-sm leading-6">
+      <label for="my-checkbox" class="select-none font-medium text-gray-900"><slot></slot></label>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "InputCheckbox",
+  props: {
+    'modelValue': {
+      type: Boolean,
+      required: true,
+    },
+  },
+  emits: [ 'update:modelValue' ],
+  computed: {
+    value: {
+      get() { return this.modelValue; },
+      set(v) { this.$emit('update:modelValue', v); },
+    },
+  },
+};
+</script>
